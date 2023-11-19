c32b77892333c3b39079f3de5361f75d0b3988c3
Matt Adewole <9janeo@gmail.com>
2023-11-19 03:14:13 -0600

    Staging (#2)
    * Modify database.yml to only include username on development environment
    
    * Ignores project notes in source control
    
    * Adds about page
    
    * Adds bootstrap 5 cdn to application, navbar partial

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  get "about", to: "about#index"
+
   resource :session
   resource :registration
   resource :password_reset
