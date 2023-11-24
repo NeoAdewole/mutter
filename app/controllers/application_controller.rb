4a6d6cf15ca4d6aee1f0443fa273ee6c7cec5b60
Matt Adewole <9janeo@gmail.com>
2023-11-24 08:29:33 -0600

    Fixes issue with current attributes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,16 @@
 class ApplicationController < ActionController::Base
+  before_action :set_current_user
+  
+  def set_current_user
+    if session[:user_id]
+      Current.user = User.find_by(id: session[:user_id])
+    end
+  end
 
   private
 
   def authenticate_user!
-    redirect_to root_path, alert: "You must be logged in to do that." unless user_signed_in?
+    redirect_to new_session_path, alert: "You must be logged in to do that." unless user_signed_in?
   end
 
   def current_user
--- app/models/current.rb
+++ app/models/current.rb
@@ -1,11 +1,5 @@
 class Current < ActiveSupport::CurrentAttributes
-  before_action :set_current_user
-
-  def set_current_user
-    if session[:user_id]
-      Current.user ||= authenticate_user_from_session
-    end
-  end
+  attribute :user
 
   def authenticate_user_from_session
     User.find_by(id: session[:user_id])
