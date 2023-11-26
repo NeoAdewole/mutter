8f057a73b044f72d70a42299e1ae34d2480ac615
Matt Adewole <9janeo@gmail.com>
2023-11-25 23:59:00 -0600

    begins adding crud actions for tweets

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,6 @@
 class TweetsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_tweet, only: [:show, :edit, :update, :destroy]
 
   def index
     @tweets = Current.user.tweets
@@ -12,16 +13,39 @@ class TweetsController < ApplicationController
   def create
     @tweet = Current.user.tweets.new(tweet_params)
     if @tweet.save
-      redirect_to @tweet, notice: "Tweet was scheduled successfully"
+      redirect_to tweets_path, notice: "Tweet was scheduled successfully"
     else
       render :new, status: 442
     end
+  end
+  
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @tweet.update(tweet_params)
+      redirect_to tweets_path, notice: "Tweet updated successfully"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @tweet.destroy
+    redirect_to tweets_path, notice: "Tweet was unscheduled"
   end
 
   private
 
   def tweet_params
     params.require(:tweet).permit(:twitter_account_id, :body, :publish_at)
+  end
+
+  def set_tweet
+    @tweet = Current.user.tweets.find(params[:id])
   end
 
 end
